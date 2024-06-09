0d3c8470d3eb0e13838272ae8ba21332b577d7ae
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-09 16:47:06 -0500

    Create Product and ProductRequest models

--- src/main/java/com/unir/store_core/model/db/Product.java
+++ src/main/java/com/unir/store_core/model/db/Product.java
@@ -1,4 +1,50 @@
 package com.unir.store_core.model.db;
 
+import com.unir.store_core.model.request.ProductRequest;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false, insertable = false, updatable = false)
+    private Category category;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    public static Product fromRequest(ProductRequest request) {
+        return Product.builder()
+                .name(request.getName())
+                .price(request.getPrice())
+                .description(request.getDescription())
+                .image(request.getImage())
+                .categoryId(request.getCategoryId())
+                .build();
+    }
 }
--- src/main/java/com/unir/store_core/model/request/ProductRequest.java
+++ src/main/java/com/unir/store_core/model/request/ProductRequest.java
@@ -1,4 +1,34 @@
 package com.unir.store_core.model.request;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRequest {
+    @NotNull(message = "`category_id` cannot be null")
+    @NotNull(message = "`category_id` cannot be empty")
+    private Long categoryId;
+
+    @NotNull(message= "`name` cannot be null")
+    @NotNull(message= "`name` cannot be empty")
+    private String name;
+
+    @NotNull(message = "`description` cannot be null")
+    @NotNull(message = "`description` cannot be empty")
+    private String description;
+
+    @NotNull(message = "`price` cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "`price` must be greater than 0")
+    private BigDecimal price;
+
+    private String image;
 }
