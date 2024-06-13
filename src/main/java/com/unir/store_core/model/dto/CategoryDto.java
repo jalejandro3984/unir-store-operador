448914b95876a35accc4071c685d8c298a8867e0
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-13 15:26:35 -0500

    Create CategoryDto

---
+++ src/main/java/com/unir/store_core/model/dto/CategoryDto.java
@@ -0,0 +1,13 @@
+package com.unir.store_core.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class CategoryDto {
+    private String name;
+}
