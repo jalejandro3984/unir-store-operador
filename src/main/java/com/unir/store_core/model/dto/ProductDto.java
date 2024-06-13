3c208e5bccfe8a68ba61b1ce7f3c1a2d3d65a717
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-13 15:26:48 -0500

    Create ProductDto

---
+++ src/main/java/com/unir/store_core/model/dto/ProductDto.java
@@ -0,0 +1,18 @@
+package com.unir.store_core.model.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ProductDto {
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private String image;
+}
