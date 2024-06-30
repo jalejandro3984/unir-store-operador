d4acd47ea0b1dea5d3ae5162ac4f153f9f06165f
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-30 16:32:17 -0500

    Eliminar encriptado

--- src/main/java/com/unir/store_core/StoreCoreApplication.java
+++ src/main/java/com/unir/store_core/StoreCoreApplication.java
@@ -1,13 +1,11 @@
 package com.unir.store_core;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class StoreCoreApplication {
 	@Bean
 	public RestTemplate restTemplate() { return new RestTemplate(); }
