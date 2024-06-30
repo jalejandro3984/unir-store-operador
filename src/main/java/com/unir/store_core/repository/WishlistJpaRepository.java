c049bdf581862081d0a6f5ea2fd1f6c3f3867944
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-30 16:37:06 -0500

    Eliminar User

--- src/main/java/com/unir/store_core/repository/WishlistJpaRepository.java
+++ src/main/java/com/unir/store_core/repository/WishlistJpaRepository.java
@@ -1,9 +1,7 @@
 package com.unir.store_core.repository;
 
-import com.unir.store_core.model.db.User;
 import com.unir.store_core.model.db.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishlistJpaRepository extends JpaRepository<Wishlist, Long> {
-    Wishlist findByUser(User user);
 }
