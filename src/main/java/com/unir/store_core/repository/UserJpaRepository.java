a9c33a3d302a762b7aca15db727cb4a5556e351d
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-30 16:31:59 -0500

    Eliminar User

--- src/main/java/com/unir/store_core/repository/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unir.store_core.repository;
-
-import com.unir.store_core.model.db.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<User, Long> {
-}
