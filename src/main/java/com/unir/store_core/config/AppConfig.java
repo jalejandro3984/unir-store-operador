7bc8a45ba1b5227bb263a884ab51f026e0513739
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-30 16:28:46 -0500

    Remover encriptacion

--- src/main/java/com/unir/store_core/config/AppConfig.java
+++ src/main/java/com/unir/store_core/config/AppConfig.java
@@ -1,13 +1,22 @@
 package com.unir.store_core.config;
 
-import org.jasypt.util.password.BasicPasswordEncryptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public BasicPasswordEncryptor passwordEncryptor() {
-        return new BasicPasswordEncryptor();
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").
+                        allowedMethods("GET", "POST", "PATCH", "DELETE").
+                        allowedHeaders("*").
+                        allowedOrigins("*");
+            }
+        };
     }
 }
