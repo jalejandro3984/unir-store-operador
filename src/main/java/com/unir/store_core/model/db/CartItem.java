ee5c18aac5eaed5259a8036a8200366f5ea709d6
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-06-30 16:35:29 -0500

    Crear relacion CartItems

---
+++ src/main/java/com/unir/store_core/model/db/CartItem.java
@@ -0,0 +1,28 @@
+package com.unir.store_core.model.db;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer qty;
+}
